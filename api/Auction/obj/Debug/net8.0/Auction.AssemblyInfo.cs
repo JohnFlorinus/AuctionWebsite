a1494937af113e3da98dfb76ef2a01ead971a039
John Florinus <johnflorinus@gmail.com>
2025-10-17 13:55:41 +0200

    fixes

--- api/Auction/obj/Debug/net8.0/Auction.AssemblyInfo.cs
+++ api/Auction/obj/Debug/net8.0/Auction.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Auction")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51ff39a1bdbf3a9e67bd9bb4ca47a06ce62ac9e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6caea809cf016bf8d744cc1bb2188bef1ce10dca")]
 [assembly: System.Reflection.AssemblyProductAttribute("Auction")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Auction")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
